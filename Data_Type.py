3657faa83e42b045438b404d3f8f7d46ae565539
saif0726 <112571017+saif0726@users.noreply.github.com>
2022-09-06 19:41:58 +0530

    Add files via upload

---
+++ Data_Type.py
@@ -0,0 +1,69 @@
+# print(type(3)) #int 
+# print(type(-9))  #int
+# print(type(5.6))  #float
+# print(type(.3))    #float
+
+# print(type('hello world'))  #string type
+# print(type("hello world"))   #string type
+# print(type('''hello world''')) #string type
+# print(type("""hello world"""))  #string type
+
+# print(type(None))  # None type
+
+# print(type(True))   # boolean type
+# print(type(False))    # boolean type
+# print(5>2)     # boolean type
+# print(5<2)  # boolean type
+
+"""List Data type"""
+# print(type([]))  #list type
+# print(type([1,2])) #list type
+# print(type([4,56]))  #list type
+
+"""Tuple Data type"""
+# a=1
+# print(a,type(a))
+# b=2
+# print(b,type(b))
+# c=4,5,6
+# print(c,type(c))
+# print(type((7,)))
+# print(type((7,8,9)))
+
+'''set data type'''
+# a=[1,2,3]
+# print(a,type(a))
+# a.clear()
+# print(a)
+
+# b={1,'hello,5.6'}
+# print(b,type(b))
+# b.clear()
+# print(b)
+
+# c={}
+# print(c,type(c))
+
+# d={1:'hello'}
+# print(d,type(d))
+# d.clear()
+# print(d)
+
+# print(type('hello'))
+# print(type(b'hello'))
+# print(type(r'hello\new'))
+# print('hello\new')
+
+# a='python'
+# print(f'hello{a}') #fast string
+
+# print(type(2+3j))
+# print(type(4j))
+# a=6
+# print/(float(a))
+# print('%f' %a)
+# print('%4f'  %a)
+# b=0.2+0.4
+# print('%f'%b)
+# print('%.1f'%b)
+# print('%.2f'%b)
